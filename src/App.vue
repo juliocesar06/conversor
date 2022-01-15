4c4f98c358b0a974b0a2f67cf29893b0c1d20c31
Julio <julio06cesar11@gmail.com>
2022-01-15 17:47:56 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>CONVERSOR MOEDA</h1>
+    
+    <conversor moeda-a="USD" moeda-b="BRL"/>
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

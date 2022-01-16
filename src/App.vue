503982bd463c5fabd3d66f15916f1cfa60b8745e
Julio <julio06cesar11@gmail.com>
2022-01-16 01:30:29 -0300

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>CONVERSOR MOEDA</h1>
+  <div id='ini'>
+    <h1 id='title'>CONVERSOR MOEDA</h1>
     <conversor/>
   </div>
 </template>
@@ -17,6 +17,23 @@ export default {
 </script>
 
 <style>
+#ini
+{
+  background-image:url("https://s.yimg.com/ny/api/res/1.2/KCnIKG6qegEzI9uYSv0rJA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2021-10/fc417280-31d7-11ec-bfff-6f1dc2c1f045");
+  
+  position: absolute;
+  top: 0;  
+  left: 0;
+  height: 610px;
+
+}
+#title
+{
+  color: black;
+  font-size: 40px;
+  margin-bottom: -5px;
+  margin-top: 5px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
